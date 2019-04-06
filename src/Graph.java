c8fea01f23b5c055836004a31097eded7828ee41
nilecochen <nile.cochen@ashbury.ca>
2019-04-06 15:11:57 -0400

    Added the Vertice and Edge sub classes to Graph. Added the data, edges, vertice1, vertice2, and weight sub objects to graph. Added the Vertice constructor, addEdge, Edge constructor, getWeight and getOther sub methods. Added vertices arraylist.

--- src/Graph.java
+++ src/Graph.java
@@ -1,2 +1,44 @@
-public class Graph {
+import java.util.ArrayList;
+
+public class Graph<T> {
+
+    private ArrayList<Vertice> vertices = new ArrayList<>();
+    private class Vertice {
+        private T data;
+        private ArrayList<Edge> edges = new ArrayList<>();
+        public Vertice(T value) {
+            data = value;
+        }
+        public void addEdge(Edge edge) {
+            edges.add(edge);
+        }
+
+    }
+
+    private class Edge {
+        private Vertice vertice1;
+        private Vertice vertice2;
+        private int weight;
+        public Edge(Vertice vertice1, Vertice vertice2, int weight) {
+            this.vertice1 = vertice1;
+            vertice1.addEdge(this);
+            this.vertice2 = vertice2;
+            vertice2.addEdge(this);
+            this.weight = weight;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public Vertice getOther(Vertice verticeCheck) {
+            if (verticeCheck.equals(vertice1)) {
+                return vertice2;
+            } else {
+                return vertice1;
+            }
+
+        }
+    }
+
 }
