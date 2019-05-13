ba7780b0a59e650942d30c03c7763465b34a9302
nilecochen <nile.cochen@ashbury.ca>
2019-05-12 22:18:28 -0400

    Added the Linkerator class: -Linkerator holds tile1 Tile, tile2 Tile, and weight int -constructor defines tile1, tile2, and weight. If the weight is less than 0, the weight is set to the distance between the two points. -getTile1, getTile2, and getWeight getter methods were added

---
+++ src/Linkerator.java
@@ -0,0 +1,28 @@
+public class Linkerator {
+    private Tile tile1;
+    private Tile tile2;
+    private int weight;
+
+    public Linkerator(Tile tile1, Tile tile2, int weight) {
+        this.tile1 = tile1;
+        this.tile2 = tile2;
+        if (weight < 0) {
+            int x = Math.abs(tile1.getX()-tile2.getX());
+            int y = Math.abs(tile1.getY()-tile2.getY());
+            weight = (int)Math.sqrt(x*x + y*y * 1.0);
+        }
+        this.weight = weight;
+    }
+
+    public Tile getTile1() {
+        return tile1;
+    }
+
+    public Tile getTile2() {
+        return tile2;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
